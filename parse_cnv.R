b92531c277400cb2de60e55a7622f5a574117a79
Raphael Saldanha <rfsaldanha@gmail.com>
2021-08-06 13:36:32 -0300

    Duplica linhas

--- parse_cnv.R
+++ parse_cnv.R
@@ -10,8 +10,13 @@ res <- readr::read_fwf(
   col_positions = readr::fwf_positions(
     start = c(1, 5, 10, 61),
     end = c(3, 9, 60, 80),
-    col_names = c("A", "B", "C", "D")
+    col_names = c("sub", "seq", "label", "cod")
   ),
   col_types = "cccc",
   locale = locale(encoding = "latin1")
-)
+) %>%
+  group_by(seq) %>%
+  tidyr::expand(cod = strsplit(x = cod, split = ",")[[1]], tidyr::nesting(sub, seq, label)) %>%
+  ungroup() %>%
+  relocate(cod, .after = label)
+
