03b9aa75c3f9737d081d2341f3e349c43c06ac9f
Raphael Saldanha <rfsaldanha@gmail.com>
2021-08-10 18:10:13 -0300

    mais testes

--- parse_cnv.R
+++ parse_cnv.R
@@ -2,37 +2,46 @@ parse_cnv <- function(file){
   
 }
 
-file <- "systems/CNV/RACACOR.CNV"
+file <- "systems/CNV/MICROBR.CNV"
 
 res <- readr::read_fwf(
   # Read CNV file with positions
   file = file,
   skip = 1,
   col_positions = readr::fwf_positions(
-    start = c(1, 5, 10, 61),
-    end = c(3, 9, 60, 80),
+    start = c(1, 4, 10, 61),
+    end = c(3, 9, 60, 1000),
     col_names = c("sub", "seq", "label", "cod")
   ),
   col_types = "cccc",
-  locale = locale(encoding = "latin1")
+  locale = readr::locale(encoding = "latin1")
 ) %>%
   # Expand rows where cod have commas
   group_by(row_number()) %>%
   tidyr::expand(cod = strsplit(x = cod, split = ",")[[1]], tidyr::nesting(sub, seq, label)) %>%
-  ungroup() %>%
-  relocate(cod, .after = label) %>%
+  dplyr::ungroup() %>%
+  dplyr::relocate(cod, .after = label) %>%
   # Create sequencies where cod have dash...
-  #mutate(cod = paste(seq(from = strsplit(x = cod, split = "-")[[1]][1], to = strsplit(x = cod, split = "-")[[1]][length(strsplit(x = cod, split = "-")[[1]])]), collapse = ",")) %>%
-  group_by(row_number()) %>%
-  mutate(cod = ifelse(test = str_detect(string = cod, pattern = "-"), yes = paste(seq(from = strsplit(x = teste, split = "-")[[1]][1], to = strsplit(x = teste, split = "-")[[1]][length(strsplit(x = teste, split = "-")[[1]])]), collapse = ","), no = cod)) %>%
-  ungroup() %>%
-  # ... and expand rows where cod have commas again
-  group_by(row_number()) %>%
-  tidyr::expand(cod = strsplit(x = cod, split = ",")[[1]], tidyr::nesting(sub, seq, label)) %>%
-  ungroup() %>%
-  relocate(cod, .after = label) %>%
-  # Remove artifical row number
-  select(-1)
+  dplyr::group_by(row_number()) %>%
+  dplyr::mutate(from_dash = ifelse(test = str_detect(string = cod, pattern = "-"), yes = TRUE, no = FALSE)) %>%
+  dplyr::mutate(cod = ifelse(test = str_detect(string = cod, pattern = "-"), yes = paste(seq(from = strsplit(x = cod, split = "-")[[1]][1], to = strsplit(x = cod, split = "-")[[1]][length(strsplit(x = cod, split = "-")[[1]])]), collapse = ","), no = cod)) %>%
+  dplyr::ungroup() %>%
+  # ... and expand again rows where cod have commas
+  dplyr::group_by(row_number()) %>%
+  tidyr::expand(cod = strsplit(x = cod, split = ",")[[1]], tidyr::nesting(sub, seq, label, from_dash)) %>%
+  dplyr::ungroup() %>%
+  dplyr::relocate(cod, .after = label) %>%
+  # Remove possible conflicts
+  dplyr::group_by(cod) %>%
+  dplyr::mutate(freq = n()) %>%
+  dplyr::filter(!(from_dash == TRUE & freq >= 2)) %>%
+  dplyr::ungroup() %>%
+  # Remove helpers
+  dplyr::select(-1, -sub, -from_dash, -freq) %>%
+  # Remove lines with NA
+  na.omit() %>%
+  # Convert cod to numeric
+  dplyr::mutate(cod = as.numeric(cod))
   
 
 mutate(cod = ifelse(str_detect(string = cod, pattern = "-"), "tem -", "nao tem -"))
